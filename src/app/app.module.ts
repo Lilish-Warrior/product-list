bc84e7a7854ef7db0428498ed303b8b37dfdf3c5
Lilish-Warrior <87679658+Lilish-Warrior@users.noreply.github.com>
2023-05-01 12:35:18 +1000

    Created custom pipe to transform characters into spaces

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { FormsModule } from '@angular/forms';
+import { CovertoSpacesPipe } from './shared/convert-to-spaces.pipe';
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent], //Our components, directives and pipes are declared here
+  declarations: [AppComponent, ProductListComponent, CovertoSpacesPipe], //Our components, directives and pipes are declared here
   imports: [BrowserModule, AppRoutingModule, FormsModule], //Components, directived and pipes from other sources(external modules) are imported here
   providers: [],
   bootstrap: [AppComponent],
