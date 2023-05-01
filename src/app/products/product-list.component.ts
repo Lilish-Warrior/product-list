bb089bf83a5b5be9dab5ab300f7218cdb7235a02
Lilish-Warrior <87679658+Lilish-Warrior@users.noreply.github.com>
2023-05-01 12:26:42 +1000

    Add OnInit to component

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
 
 @Component({
   selector: 'pm-products',
   templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.scss'],
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
   pageTitle: string = 'Product List';
   imageWidth: number = 50;
   imageMargin: number = 2;
@@ -36,5 +37,9 @@ export class ProductListComponent {
 
   displayImage(): void {
     this.showImage = !this.showImage;
+  }
+
+  ngOnInit(): void {
+    console.log('ng onInit');
   }
 }
