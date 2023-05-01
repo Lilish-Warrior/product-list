f8ea801f78b63cc504e40fd9f3180877e7634dd7
Lilish-Warrior <87679658+Lilish-Warrior@users.noreply.github.com>
2023-05-01 12:08:15 +1000

    Add interpolation in the template, pipes and ngModule to bind data from component into template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, ProductListComponent], //Our components, directives and pipes are declared here
+  imports: [BrowserModule, AppRoutingModule, FormsModule], //Components, directived and pipes from other sources(external modules) are imported here
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -4,4 +4,36 @@ import { Component } from '@angular/core';
   selector: 'pm-products',
   templateUrl: './product-list.component.html',
 })
-export class ProductListComponent {}
+export class ProductListComponent {
+  pageTitle: string = 'Product List';
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  showImage: boolean = false;
+  listFilter: string = 'cart';
+  products: any[] = [
+    {
+      productId: 2,
+      productName: 'Garden Cart',
+      productCode: 'GDN-0023',
+      releaseDate: 'March 18, 2023',
+      description: ' Rolling garden cart',
+      price: 32.99,
+      starRating: 4.2,
+      imageURL: 'assets/images/garden_cart.png',
+    },
+    {
+      productId: 5,
+      productName: 'Hammer',
+      productCode: 'TBX-0498',
+      releaseDate: 'May 21, 2021',
+      description: ' Curved Hammer',
+      price: 8.9,
+      starRating: 4.8,
+      imageURL: 'assets/images/hammer.png',
+    },
+  ];
+
+  displayImage(): void {
+    this.showImage = !this.showImage;
+  }
+}
