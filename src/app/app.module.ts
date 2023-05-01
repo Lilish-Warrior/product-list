ab0b0c85659b1b21d3ffb024147a8afdf0ab9cf2
Lilish-Warrior <87679658+Lilish-Warrior@users.noreply.github.com>
2023-05-01 14:06:00 +1000

    Created star rating component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,15 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { FormsModule } from '@angular/forms';
 import { CovertoSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { StarComponent } from './shared/star-component/star.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent, CovertoSpacesPipe], //Our components, directives and pipes are declared here
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    CovertoSpacesPipe,
+    StarComponent,
+  ], //Our components, directives and pipes are declared here
   imports: [BrowserModule, AppRoutingModule, FormsModule], //Components, directived and pipes from other sources(external modules) are imported here
   providers: [],
   bootstrap: [AppComponent],
