477ba706dfa506372efac86c17d545e356e98d0d
Lilish-Warrior <87679658+Lilish-Warrior@users.noreply.github.com>
2023-05-01 12:54:28 +1000

    Add getter and setter to define the products filtered by user. Add filter method to filter products as user types to search

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -11,7 +11,19 @@ export class ProductListComponent implements OnInit {
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
-  listFilter: string = 'cart';
+
+  private _listFilter: string = '';
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    console.log('In setter:', value);
+    this.filteredProducts = this.performFilter(value);
+  }
+
+  filteredProducts: IProduct[] = [];
+
   products: IProduct[] = [
     {
       productId: 2,
@@ -35,11 +47,18 @@ export class ProductListComponent implements OnInit {
     },
   ];
 
+  performFilter(filteredBy: string): IProduct[] {
+    filteredBy = filteredBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) =>
+      product.productName.toLocaleLowerCase().includes(filteredBy)
+    );
+  }
+
   displayImage(): void {
     this.showImage = !this.showImage;
   }
 
   ngOnInit(): void {
-    console.log('ng onInit');
+    this.listFilter = 'cart';
   }
 }
