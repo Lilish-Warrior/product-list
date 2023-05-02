dfe6c6e528df086f3dab5090205db0093576bf13
Lilish-Warrior <87679658+Lilish-Warrior@users.noreply.github.com>
2023-05-02 12:39:22 +1000

    Add product service to website

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { IProduct } from './product';
 import { ProductService } from './product.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'pm-products',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss'],
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnDestroy {
   pageTitle: string = 'Product List';
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
+  errorMessage: string = '';
+  sub!: Subscription;
 
   private _listFilter: string = '';
   get listFilter(): string {
@@ -41,11 +44,33 @@ export class ProductListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.products = this.productService.getProducts();
-    this.filteredProducts = this.products;
+    this.sub = this.productService.getProducts().subscribe({
+      next: (products) => {
+        this.products = products;
+        this.filteredProducts = this.products;
+      },
+      error: (err) => (this.errorMessage = err),
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
   }
 
   onRatingClicked(message: string): void {
     this.pageTitle = 'Product List: ' + message;
   }
 }
+
+//Async operation:
+//1st: Angular initialises component in ngOnInit
+//2nd: We call getProducts() from the productService.ts
+//3rd: The productService.ts returns an observable of type IProduct[]
+//4th: We substribe to that observable (inside ngOnInit) and the HTTP GET request is submitted
+//Asyn operation ends here
+//5th: We set filterProducts in this.filteredProducts. But at this point, since we haven't received a response, the filteredProducts is an empty array.
+//At a future point in time:
+//1st: The service receives the HTTP response from our request: The response data is mapped to an array of products
+//2nd: The response is piped (you can see that in product.service.ts) through any operators, (in our example: tap and catchError) and the observable emits that mapped data to any subscriber.
+//3rd: Our subscriber (ngOnInit in product-list.component.ts) receives the emitted data and
+//4thL assigns our product property to the emitted array of products.
--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,42 +1,35 @@
 import { Injectable } from '@angular/core';
 import { IProduct } from './product';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, catchError, tap, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
-  getProducts(): IProduct[] {
-    return [
-      {
-        productId: 2,
-        productName: 'Garden Cart',
-        productCode: 'GDN-0023',
-        releaseDate: 'March 18, 2023',
-        description: ' Rolling garden cart',
-        price: 32.99,
-        starRating: 4.2,
-        imageURL: 'assets/images/garden_cart.png',
-      },
-      {
-        productId: 5,
-        productName: 'Hammer',
-        productCode: 'TBX-0498',
-        releaseDate: 'May 21, 2021',
-        description: ' Curved Hammer',
-        price: 8.9,
-        starRating: 4.8,
-        imageURL: 'assets/images/hammer.png',
-      },
-      {
-        productId: 4,
-        productName: 'Piano',
-        productCode: 'CFR-0689',
-        releaseDate: 'May 28, 2019',
-        description: ' Great Piano',
-        price: 90.5,
-        starRating: 4.9,
-        imageURL: 'assets/images/piano.png',
-      },
-    ];
+  private _productUrl = 'src/api/products/products.json';
+  constructor(private http: HttpClient) {}
+
+  getProducts(): Observable<IProduct[]> {
+    return this.http.get<IProduct[]>(this._productUrl).pipe(
+      tap((data) => console.log('All', JSON.stringify(data))),
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(err: HttpErrorResponse) {
+    //In real world app, we may send the server to some remote logging infrastructure
+    // instead of just logging it to the console.
+    let errorMessage = '';
+    if (err.error instanceof ErrorEvent) {
+      //A client-side or network ocurred. Handle it accordingly.
+      errorMessage = `An error ocurred: ${err.error.message}`;
+    } else {
+      //The backend returned an unsuccesful responde code.
+      // THe response body may contain clues as to what went wrong,
+      errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+    }
+    console.error(errorMessage);
+    return throwError(() => errorMessage);
   }
 }
